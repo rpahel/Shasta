660e8e11bd31b5b16509064c949e604812f47dfd
rpahel <92730433+rpahel@users.noreply.github.com>
2024-08-16 20:15:33 +0200

    Generation de cells

--- Source/Shasta/Private/Actors/Cells/WorldCell.cpp
+++ Source/Shasta/Private/Actors/Cells/WorldCell.cpp
@@ -3,6 +3,7 @@
 
 #include "Actors/Cells/WorldCell.h"
 #include "Components/ShapeComponent.h"
+#include <Logging/StructuredLog.h>
 
 //====================================================================================
 //==== PUBLIC CONSTRUCTORS
@@ -11,14 +12,126 @@
 AWorldCell::AWorldCell()
 {
 	RootComponent = CreateDefaultSubobject<USceneComponent>("Root");
+
+	Pivot = CreateDefaultSubobject<USceneComponent>("Rotate me");
+	if (Pivot)
+		Pivot->SetupAttachment(RootComponent);
+
 	DissolverShape = CreateDefaultSubobject<UStaticMeshComponent>("Dissolver Shape");
 	if(DissolverShape)
 		DissolverShape->SetupAttachment(RootComponent);
+
+	Neighbors.Empty();
+	Neighbors.Reserve(6);
+	SideCenters.Empty();
+	SideCenters.Reserve(6);
+	for (const FIntPoint& angle : { FIntPoint(0, 60), FIntPoint(60, 120), FIntPoint(120, 180), FIntPoint(180, 240), FIntPoint(240, 300), FIntPoint(300, 360) })
+	{
+		Neighbors.Add(angle, nullptr);
+		SideCenters.Add(angle, FMath::Lerp(FVector::RightVector.RotateAngleAxis(angle.X, FVector::UpVector), FVector::RightVector.RotateAngleAxis(angle.Y, FVector::UpVector), 0.5f));
+	}
 }
 
 //====================================================================================
-//==== PROTECTED METHODS
+//==== PRIVATE OVERRIDES
 //====================================================================================
+
+void AWorldCell::Init(AWorldCell* parentCell)
+{
+	if(!parentCell)
+		return;
+
+	SetDistanceFromCenter(parentCell->GetDistanceFromCenter() + 1);
+}
+
+int32 AWorldCell::GetDistanceFromCenter() const
+{
+	return DistanceFromCenter;
+}
+
+const TMap<FIntPoint, TObjectPtr<AWorldCell>>& AWorldCell::GetNeighbors() const
+{
+	return Neighbors;
+}
+
+void AWorldCell::GenerateNeighbors()
+{
+	FActorSpawnParameters spawnParams;
+	spawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	for (auto& pair : Neighbors)
+	{
+		if(pair.Value)
+			continue;
+
+		pair.Value = GetWorld()->SpawnActor<AWorldCell>(
+			WorldCellTemplate,
+			FTransform(GetActorLocation() + 2 * SideCenters[pair.Key] * CellRadius),
+			spawnParams
+		);
+
+		if (!pair.Value)
+		{
+			UE_LOGFMT(LogTemp, Warning, "{0} : Could not generate neighbour at {1}", GetName(), pair.Key.ToString());
+			continue;
+		}
+	}
+}
+
+//====================================================================================
+//==== PRIVATE OVERRIDES
+//====================================================================================
+
+void AWorldCell::BeginPlay()
+{
+	if (bDrawDebugs)
+	{
+		FColor colorA(FColor::MakeRandomColor());
+		FColor colorB(FColor::MakeRandomColor());
+		for (auto& neighbor : Neighbors)
+		{
+			DrawDebugLine(
+				GetWorld(),
+				GetActorLocation(),
+				GetActorLocation() + FVector::RightVector.RotateAngleAxis(neighbor.Key.X, FVector::UpVector) * CellRadius,
+				colorA,
+				true
+			);
+
+			DrawDebugLine(
+				GetWorld(),
+				GetActorLocation() + FVector::RightVector.RotateAngleAxis(neighbor.Key.X, FVector::UpVector) * CellRadius,
+				GetActorLocation() + FVector::RightVector.RotateAngleAxis(neighbor.Key.Y, FVector::UpVector) * CellRadius,
+				colorA,
+				true
+			);
+
+			DrawDebugLine(
+				GetWorld(),
+				GetActorLocation(),
+				GetActorLocation() + FVector::RightVector.RotateAngleAxis(neighbor.Key.Y, FVector::UpVector) * CellRadius,
+				colorA,
+				true
+			);
+
+			DrawDebugLine(
+				GetWorld(),
+				GetActorLocation(),
+				GetActorLocation() + SideCenters[neighbor.Key] * CellRadius,
+				colorB,
+				true
+			);
+		}
+	}
+}
+
+//====================================================================================
+//==== PRIVATE METHODS
+//====================================================================================
+
+void AWorldCell::SetDistanceFromCenter(int Distance)
+{
+	DistanceFromCenter = Distance;
+}
 
 UStaticMeshComponent* AWorldCell::GetDissolverShape() const
 {
--- Source/Shasta/Public/Actors/Cells/WorldCell.h
+++ Source/Shasta/Public/Actors/Cells/WorldCell.h
@@ -17,7 +17,7 @@ enum class ECellType : uint8
 	None	= 0,
 	Border	= 1,
 	Defense	= 2,
-	Heart	= 4
+	Center	= 4
 };
 
 USTRUCT()
@@ -70,20 +70,58 @@ private:
 	UPROPERTY(EditDefaultsOnly, Category = "Shasta|World Cell")
 	FDissolverShapeData DissolverShapeData;
 
+	UPROPERTY(EditDefaultsOnly, Category = "Shasta|World Cell")
+	TSubclassOf<AWorldCell> WorldCellTemplate;
+
 	//==== Hidden Fields ====
 
-	UPROPERTY(VisibleAnywhere, BlueprintGetter = GetDissolverShape)
-	TObjectPtr<UStaticMeshComponent> DissolverShape;
+	UPROPERTY(EditAnywhere, Category = "Shasta|World Cell|Debug")
+	bool bDrawDebugs = false;
+
+	UPROPERTY(VisibleAnywhere, Category = "Shasta|World Cell|Debug")
+	float CellRadius = 100;
+
+	UPROPERTY(VisibleAnywhere, Category = "Shasta|World Cell|Debug")
+	TMap<FIntPoint, TObjectPtr<AWorldCell>> Neighbors; // Neighbors, world space, counter clockwise
+
+	UPROPERTY(VisibleAnywhere, Category = "Shasta|World Cell|Debug")
+	TMap<FIntPoint, FVector> SideCenters; // local space, counter clockwise
+
+	UPROPERTY(VisibleAnywhere, Category = "Shasta|World Cell|Debug")
+	int32 DistanceFromCenter = 0;
 
 	TMultiMap<EShastaPathType, TObjectPtr<UPathComponent>> Paths;
+
+	//==== Components ====
+
+	UPROPERTY(VisibleAnywhere, Category = "Shasta|World Cell|Debug")
+	TObjectPtr<USceneComponent> Pivot;
+
+	UPROPERTY(VisibleAnywhere, BlueprintGetter = GetDissolverShape, Category = "Shasta|World Cell|Debug")
+	TObjectPtr<UStaticMeshComponent> DissolverShape;
 
 public:
 	//==== Constructors ====
 
 	AWorldCell();
 
-protected:
 	//==== Methods ====
+
+	void Init(AWorldCell* parentCell);
+	int32 GetDistanceFromCenter() const;
+	const TMap<FIntPoint, TObjectPtr<AWorldCell>>& GetNeighbors() const;
+
+	UFUNCTION(CallInEditor, Category = "Shasta")
+	void GenerateNeighbors();
+
+private:
+	//==== Overrides ====
+
+	void BeginPlay() override;
+
+	//==== Methods ====
+
+	void SetDistanceFromCenter(int Distance);
 
 	UFUNCTION(BlueprintCallable, BlueprintPure)
 	UStaticMeshComponent* GetDissolverShape() const;
